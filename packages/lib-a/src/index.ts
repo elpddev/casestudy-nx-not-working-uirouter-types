2a47ea2e05480a321eaa80ac7fc7d8e71623dda1
eyall@proftit.com <eyall@proftit.com>
2022-01-09 08:49:51 +0200

    casestudy of uirouter type import problem

--- packages/lib-a/src/index.ts
+++ packages/lib-a/src/index.ts
@@ -1,2 +1,14 @@
-export const a = 1;
+import ng, { ui } from 'angular';
 
+export const AppModule = ng.module('app', ['ui.router']);
+
+AppModule.config([
+    '$stateProvider',
+    ($stateProvider: ui.IStateProvider) => {
+      $stateProvider
+        .state('main', {
+          url: '/main',
+          template: '<ui-view/>',
+        });
+    }
+]);
